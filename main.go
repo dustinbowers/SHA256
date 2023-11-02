9794db08c5f41fd1cfc01824d27a0ba90239c09f
Dustin Bowers <dustin.uncc@gmail.com>
2023-11-01 22:07:47 -0400

    Cleanup

--- main.go
+++ main.go
@@ -4,27 +4,31 @@ import (
 	"SHA256/hash"
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Incorrect arguments.\nUsage: sha255 <filename>")
+	if len(os.Args) == 1 || len(os.Args) > 2 {
+		fmt.Println("Usage: SHA256 <filename>")
+		os.Exit(1)
 		return
 	}
+	inputFile := os.Args[1]
 
-	f, err := os.Open(os.Args[1])
+	f, err := os.Open(inputFile)
 	if err != nil {
-		log.Fatalf("Error reading file [%v]: %v\n", os.Args[1], err.Error())
+		fmt.Printf("Error reading file [%v]: %v\n", inputFile, err.Error())
+		os.Exit(1)
 	}
+	defer f.Close()
 
+	r := bufio.NewReader(f)
 	h := hash.NewSHA256()
-	digest, err := h.Sum(bufio.NewReader(f))
+	digest, err := h.Sum(r)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%x  %s\n", digest, os.Args[1])
+	fmt.Printf("%x  %s\n", digest, inputFile)
 }
